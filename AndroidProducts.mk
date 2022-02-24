c0e67628790e0f7044c1341a4689c80ae33dae17
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-24 22:56:04 +0700

    maple_dsds: Nusantara-12 Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_maple_dsds.mk
+    $(LOCAL_DIR)/nad_maple_dsds.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_maple_dsds-user \
-    lineage_maple_dsds-userdebug \
-    lineage_maple_dsds-eng
+    nad_maple_dsds-user \
+    nad_maple_dsds-userdebug \
+    nad_maple_dsds-eng
