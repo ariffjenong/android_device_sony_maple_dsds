08e9bdd1f5e56a965f6aceba48fbba788347f615
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-08 15:14:17 +0700

    CherishOS Bringup

--- cherish_maple_dsds.mk
+++ cherish_maple_dsds.mk
@@ -9,32 +9,28 @@ $(call inherit-product, device/sony/maple_dsds/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 ### BOOTANIMATION
-# vendor/lineage/config/common_full_phone.mk
+# vendor/cherish/config/common_full_phone.mk
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-# vendor/lineage/config/common.mk
-TARGET_BOOTANIMATION_HALF_RES := true
+# vendor/cherish/config/common.mk
+TARGET_BOOT_ANIMATION_RES := 1080
 
-### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+### cherish
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_maple_dsds
+PRODUCT_NAME := cherish_maple_dsds
 PRODUCT_DEVICE := maple_dsds
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8142
 PRODUCT_MANUFACTURER := Sony
+
+# Cherish OS Stuffs
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer= Arif JeNong
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=maple_dsds \
     PRIVATE_BUILD_DESC="G8142-user 9 47.2.A.10.107 172320177 release-keys"
 
 BUILD_FINGERPRINT := Sony/G8142/G8142:9/47.2.A.10.107/172320177:user/release-keys
-
-ifneq ($(LINEAGE_DEV_CERTIFICATE),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
-endif
-
-ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
-    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
-endif
